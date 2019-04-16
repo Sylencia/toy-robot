ad8fa3e7b0fd4c3b022a0d8a054f7ed1758bf974
Joseph <valrigardian@gmail.com>
2019-04-17 01:35:15 +1000

    Updates

---
+++ precache-manifest.7a3e2a804afece157142f1bf291fb106.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4d42f616128a5514b7b",
+    "url": "/toy-robot/static/js/runtime~main.d65810f7.js"
+  },
+  {
+    "revision": "b9adcaa920ed5e6d74d1",
+    "url": "/toy-robot/static/js/main.b02c647f.chunk.js"
+  },
+  {
+    "revision": "9cf5e738809cf45b6efc",
+    "url": "/toy-robot/static/js/2.1b9eed8a.chunk.js"
+  },
+  {
+    "revision": "b9adcaa920ed5e6d74d1",
+    "url": "/toy-robot/static/css/main.b4e61537.chunk.css"
+  },
+  {
+    "revision": "15cc69dc97f294c60c1a0ae573101ff5",
+    "url": "/toy-robot/index.html"
+  }
+];
